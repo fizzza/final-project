8ac5def975f62e7cf2cb6b6ab15b91bff62cd4e5
Fizza Usman <ec221009@qmul.ac.uk>
2024-01-19 12:35:39 +0000

    time series graphs

--- final project.py
+++ final project.py
@@ -41,23 +41,4 @@ plt.title('Time Series Graph: CPI for Unprocessed Food Goods Overtime  ')
 plt.grid(True)  # Show grid
 plt.show()
 
-# Assign x-values and y-values
-x_values = df.iloc[:, 0]
-y1_values = df.iloc[:, 1]
-y2_values = df.iloc[:, 2]
-y3_values = df.iloc[:, 3]
-
-# Set the bar width (adjust as needed)
-bar_width = 0.2
-
-# Plot the bar graph
-plt.bar(x_values, y1_values, width=bar_width, label='CPI for Food, Alcoholic Beverages & Tobacco Goods')
-plt.bar(x_values + bar_width, y2_values, width=bar_width, label='CPI for Processed Food & Non-Alcoholic Beverages Goods')
-plt.bar(x_values + 2 * bar_width, y3_values, width=bar_width, label='CPI for Unprocessed Food Goods')
-
-plt.xlabel('Year')
-plt.ylabel('CPI')
-plt.title('Bar Graph Example')
-plt.legend()  # Show legend if you have labels
-plt.show()
 
